0dc436df61f84cf88c48f6d6a98e8834b156c837
Henry Adam <iihenymii@gmail.com>
2024-10-18 17:18:56 -0300

    Membros adicionado, consumo api funcionando

---
+++ js/api.js
@@ -0,0 +1,20 @@
+function searchMember() {
+   let member = document.querySelector('#characters').value
+   let url = `https://gsi.fly.dev/characters/search?name=${member}`
+
+   fetch(url).then(function(response){
+        response.json().then(function(data) {
+            console.log(data)
+            displayData(data)
+        })
+   })
+
+}
+
+function displayData(data) {
+    let result = document.querySelector('#result')
+
+    result.innerHTML = `<p>Nome: ${data.results[0].name}</p>
+                        <p>Visão: ${data.results[0].vision}</p>
+                        <p>Arma: ${data.results[0].weapon}</p>`
+}
