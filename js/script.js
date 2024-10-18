2dec855696f4c31a16c994600bf45416a2f16cad
Henry Adam <iihenymii@gmail.com>
2024-10-18 12:14:25 -0300

    card slider Taberna funcionando

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+const carousel0 = document.querySelector(".carousel0")
+const carousel1 = document.querySelector(".carousel1")
+const arrowBtns = document.querySelectorAll(".arrow")
+const arrowBtns1 = document.querySelectorAll(".arrow1")
+const dishesCardWidth = carousel0.querySelector(".card").offsetWidth
+const drinksCardWidth = carousel1.querySelector(".card").offsetWidth
+
+
 function showSidebar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'flex'
@@ -7,3 +15,15 @@ function hideSidebar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
 }
+
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carousel0.scrollLeft += btn.id === "left" ? - dishesCardWidth : dishesCardWidth
+    })
+})
+
+arrowBtns1.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carousel1.scrollLeft += btn.id === "left1" ? - drinksCardWidth : drinksCardWidth
+    })
+})
