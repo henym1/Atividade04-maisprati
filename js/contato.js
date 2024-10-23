01b94da695895dbbee94b85b5f36dc41a53e36d7
Henry Adam <iihenymii@gmail.com>
2024-10-23 15:04:33 -0300

    formulario Contato com verificacao

--- js/contato.js
+++ js/contato.js
@@ -8,6 +8,32 @@ function hideSidebar() {
     sidebar.style.display = 'none'
 }
 
+const emailInput = document.querySelector("#email")
+const resultMessage = document.querySelector("#resultMessage")
+const contactForm = document.querySelector("#contactForm")
+
+contactForm.addEventListener("submit", function(event) {
+    event.preventDefault()
+
+    const email = emailInput.value
+
+    const isValid = validateEmail(email)
+
+    if(isValid) {
+        sendMail()
+    } else {
+        resultMessage.textContent = "Email inválido"
+        resultMessage.style.color = "red"
+        resultMessage.style.fontWeight = 700
+    }
+})
+
+const validateEmail = (email) => {
+    const regex = /^[^\s]+@[^\s]+\.+[^\s]+$/
+    return regex.test(email)
+}
+
+
 function sendMail() {
     let parms = {
         name : document.getElementById("nome").value,
@@ -17,4 +43,12 @@ function sendMail() {
     }
 
     emailjs.send("service_yr47njy","template_am2jjop",parms).then(alert("Mensagem enviada!"))
+    clearForm()
+}
+
+function clearForm() {
+    document.getElementById("nome").value = ""
+    document.getElementById("sobrenome").value = ""
+    document.getElementById("email").value = ""
+    document.getElementById("mensagem").value = ""
 }
--- js/membros.js
+++ js/membros.js
@@ -1,3 +1,13 @@
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
+
 function searchMember() {
    let member = document.querySelector('#characters').value
    let url = `https://gsi.fly.dev/characters/search?name=${member}`
